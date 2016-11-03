7f0b39ca733f1ac45ba66daded4919a978babcb6
Kristoffer <krillllind@gmail.com>
2016-11-03 09:13:57 +0100

    The goodz

---
+++ TestWebApp/gruntfile.js
@@ -0,0 +1,29 @@
+﻿/*global module */
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: { "style.css": "sass/style.scss" }
+            }
+        },
+        uglify: {
+            core: { files: { 'project.core.min.js': ['scripts/core.js', 'scripts/*.js'] } },
+            vendor: { files: { 'project.vendor.min.js': ['scripts/vendor/jquery-3.1.1.min.js', 'scripts/vendor/bootstrap.js'] } }
+        },
+        watch: {
+            assets: { files: ['sass/**/*.scss'], tasks: ['sass'] },
+            scripts: { files: ['scripts/**/*.js'], tasks: ['uglify'] }
+        }
+    });
+
+    grunt.registerTask('watch', ['watch']);
+
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-sass");
+};
---
+++ TestWebApp/scripts/core.js
@@ -0,0 +1,47 @@
+﻿var canPlayPause = true;
+
+$(document).ready(function () {
+    var p = $("#video_player");
+    p.prop("volume", 1)
+
+    $(".video_container > .overlay").click(function (e) {
+        playPause($(this), p);
+    });
+
+    document.addEventListener("keyup", function (e) {
+        if (e.keyCode == 75)
+            playPause($(".video_container > .overlay").get(0), p);
+    });
+});
+
+function playPause(el, player)
+{
+    if (canPlayPause)
+    {
+        canPlayPause = false;
+
+        var playVideo = $(el).data("play");
+
+        if (playVideo == true) {
+            $(el).stop().animate({
+                opacity: 0
+            }, 500, function () {
+                player.get(0).play();
+                $(el).html("<i class=\"fa fa-pause-circle-o col-xs-12\" aria-hidden=\"true\"></i>");
+                $(el).data("play", false);
+                canPlayPause = true;
+            });
+        }
+        else {
+            player.get(0).pause();
+
+            $(el).stop().animate({
+                opacity: 1
+            }, 200, function () {
+                $(el).html("<i class=\"fa fa-play-circle-o col-xs-12\" aria-hidden=\"true\"></i>");
+                $(el).data("play", true);
+                canPlayPause = true;
+            });
+        }
+    }
+}
